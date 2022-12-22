3812fb342d987abb2f28c95803fc63abe8fccb55
aniket-vasy <aniketsen.vasyerp@gmail.com>
2022-12-22 18:08:29 +0530

    express server for paytm payments

---
+++ index.js
@@ -0,0 +1,167 @@
+require("dotenv").config();
+const process = require("process");
+const express = require("express");
+const app = express();
+const PaytmChecksum = require("./paytmChecksum");
+const cors = require("cors");
+var https = require('https');
+
+const PORT = process.env.PORT || 3001; /// setting up PORT
+///// Cors for
+app.use(
+  cors({
+    origin: "*", // allow url which can send request to this server
+    methods: ["POST", "GET"], //
+  })
+);
+///// parsing json
+app.use(express.json());
+
+
+const developers = [];
+let count = 0
+
+
+
+
+app.get("/", (req, res) => {
+  res.json({
+    name: "aniket",
+    id: "1",
+    req: req.app.get("views"),
+  });
+});
+
+// app.get("/:id", (req, res) => {
+//   if (req.params.id == 0) {
+//     res.status(404).send("Id Should not be zero in Rout Parameter");
+//   } else {
+//     res.json({
+//       Id_params: req.params.id,
+//     });
+//   }
+// });
+
+app.post("/", (req,res) => {
+  const person = {
+    id:req.body.id,
+    name:req.body.name,
+    index:count++
+  }
+  
+  developers.push(person);
+  console.log("developers",developers);
+  res.json(developers);
+})
+
+//// paytm api ---------------------------------------------========
+app.post("/payment",(req,res)=>{
+  /////////------
+  // res.send("response")
+  // let orderId = 'PYTM_ORDR_'+new Date().getTime();
+  // let orderId = `PYTM_ORDR_1670914911410`;
+
+  // Sandbox Credentials
+  // let mid = "AqSipo92499010904391"; // Merchant ID
+  // let mkey = "nCVjURN@UrBq9mnX"; // Merhcant Key
+
+  ////////// fetching details from request
+  let mid = req.body.mid;
+  let mkey = req.body.mkey;
+  let amount = req.body.amount;
+  let orderId = req.body.orderId;
+
+  var paytmParams = {};
+
+  paytmParams.body = {
+    "requestType"  : "Payment",
+    "mid"      : `${mid}`,
+    "websiteName"  : "DEFAULT",
+    "orderId"    : `${orderId}`,
+    "callbackUrl"  : `http://localhost:8081`,
+    "txnAmount"   : {
+      "value"   : `${amount}`,
+      "currency" : "INR",
+    },
+    "userInfo"   : {
+      "custId"  : '1001',
+    },
+    // "enablePaymentMode":[{"mode" : "UPI", "channels" : ["UPIPUSH"]}]
+    // "disablePaymentMode":[{"mode" : "UPI", "channels" : ["UPIPUSH"]}]
+   
+  };
+
+  PaytmChecksum.generateSignature(JSON.stringify(paytmParams.body), mkey).then(function(checksum){
+      console.log("checksum",checksum);
+    paytmParams.head = {
+      "signature" : `${checksum}`
+    };
+
+    var post_data = JSON.stringify(paytmParams);
+
+
+    console.log("paytmParams.body",paytmParams.body)
+
+    var options = {
+      // / for Staging /
+      hostname: 'securegw-stage.paytm.in',
+
+      // / for Production /
+          // hostname: 'securegw.paytm.in',
+
+      port: 443,
+      path: `/theia/api/v1/initiateTransaction?mid=${mid}&orderId=${orderId}`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': `${post_data.length}`
+      }
+    };
+    console.log("post_data.length",post_data.length)
+    var response = "";
+    var post_req = https.request(options, function(post_res) {
+      console.log("post_res",post_res)
+      post_res.on('data', function (chunk) {
+        response += chunk;
+        console.log("******res====>",response)
+        
+      });
+          post_res.on('end', function(){
+
+
+        // console.log('Response: ', response);
+        // console.log('post data: ', post_data);
+
+              // res.json({data: JSON.parse(response), orderId: orderId, mid: mid, amount: amount});
+           
+              console.log("befor-data-set")
+              // setPaymentData({
+              //     ...paymentData,
+              //     token: JSON.parse(response).body.txnToken,
+              //     order: orderId,
+              //     mid: mid,
+              //     amount: {amount}
+              // })
+              // console.log(response)
+              res.json(JSON.parse(response))
+              // console.log("after-data-set",paymentData)
+              
+             
+      });
+    });
+
+    post_req.write(post_data);
+    post_req.end();
+    
+  });
+})
+
+app.listen(PORT, () => {
+  // this will open server s
+  console.log(`listing on port ${process.env.PORT}`);
+});
+
+// const http = require("http");
+// const server = http.createServer((req,res)=>{
+//     res.end("Hello..... Message From Server Side");
+// })
